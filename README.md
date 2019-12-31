a1843a6205b0fd7837e5133f29ace2e310213468
zhaoyifan <zhaoyifans@gmail.com>
2019-12-31 16:44:46 +0800

    feat: 06 图片、文件处理

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Webpack4 Boilerplate
 
 配置一份 Webpack4 + Vue2.6 的模板项目。
+
+## References
+
+- [kaivin/webpack4.x | GitHub](https://github.com/kaivin/webpack4.x)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./css/reset.css";
 import "./scss/public.scss";
+import bgImg from "./images/spikes.png";
 
-document.getElementById("app").innerHTML = "Webpack works";
+var func = str => {
+  document.getElementById("app").innerHTML = str;
+};
+func("我现在在使用 es6 新语法-箭头函数!");
+
 document.getElementById("postcss").innerHTML =
-  "<h1>我自动添加了浏览器前缀</h1>";
+  "<h1>我自动添加了浏览器前缀</h1><img src='" + bgImg + "'/>";
